6308b90959cfa53a46c91be371154ae9913778ca
olevole <olevole@olevole.ru>
2023-01-08 16:40:13 +0300

    log output to file to not block output

---
+++ README.md
@@ -0,0 +1,9 @@
+
+Reggae init test regression
+
+Please edit CBSDfile vm_os_profile/settings or overwrite via command line:
+
+```
+cbsd up vm_os_profile="cloud-FreeBSD-ufs-x64-13.1" ip4_addr="10.0.0.2"
+```
+
--- prepare.sh
+++ prepare.sh
@@ -1,27 +1,47 @@
 #!/bin/sh
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 
+is_zfs=0
+zfs get all / > /dev/null 2>&1
+ret=$?
+[ ${ret} -eq 0 ] && is_zfs=1
+
 # exit on error
 set -o errexit
+
+trap "set +o xtrace; echo \"ERROR\"; cat /tmp/log.log" HUP INT ABRT BUS TERM EXIT
 
 # trace on
 set -o xtrace
 
-pkg update -f
-pkg install -y reggae
+###
+pkg update -f > /tmp/log.log 2>&1
+pkg install -y reggae > /tmp/log.log 2>&1
 
 hash -r
 
 grep -v Commented /usr/local/etc/reggae.conf.sample | sed 's:# ::g' > /usr/local/etc/reggae.conf
 
-reggae network-init
-service pflog restart
-service pf restart
-reggae cbsd-init
-env NOINTER=1 reggae master-init
+if [ ${is_zfs} -eq 1 ]; then
+	# for ZFS
+	sysrc -qf /usr/local/etc/reggae.conf ZFS_POOL="tank" USE_ZFS="yes"
+else
+	# for UFS
+	sysrc -qf /usr/local/etc/reggae.conf ZFS_POOL="" USE_ZFS="no"
+fi
+
+reggae network-init > /tmp/log.log 2>&1
+cat /tmp/log.log
+service pflog restart > /tmp/log.log 2>&1
+cat /tmp/log.log
+service pf restart > /tmp/log.log 2>&1
+cat /tmp/log.log
+reggae cbsd-init > /tmp/log.log 2>&1
+cat /tmp/log.log
+env NOINTER=1 reggae master-init > /tmp/log.log 2>&1
+cat /tmp/log.log
 
 # trace off
 set +o xtrace
 
-echo "Reggae init complete"
 exit 0
